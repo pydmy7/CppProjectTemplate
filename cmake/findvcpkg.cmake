032d7f5d4b28f4e0a7f545e6af4be4d0f4414590
wangyao <1822364545@qq.com>
2024-07-21 20:35:26 +0800

    vcpkg

---
+++ cmake/findvcpkg.cmake
@@ -0,0 +1,6 @@
+if(DEFINED ENV{VCPKG_ROOT})
+    set(CMAKE_TOOLCHAIN_FILE "$ENV{VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" CACHE STRING "")
+else()
+    message(FATAL_ERROR "VCPKG_ROOT is not defined")
+    message("Usage: -D CMAKE_TOOLCHAIN_FILE=<you vcpkg root dir>/scripts/buildsystems/vcpkg.cmake")
+endif()
